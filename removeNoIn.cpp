2b5007b0d2e937ec4692e07e8077c41388e37e16
Henrieta Micheľová <xmichel@fi.muni.cz>
2021-02-25 21:25:42 +0100

    First roads with init car

--- removeNoIn.cpp
+++ removeNoIn.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <set>
 #include <map>
+#include <algorithm>
 
 
 
@@ -18,9 +19,39 @@ struct Intersection
 {
 	int index;
 	std::vector<std::string> street_names;
+    std::map<std::string, int> nameToCount;
 	std::set<std::string> street_names_used;
 	explicit Intersection(int index):
 		index(index) {}
+
+    void initToCount() {
+        for (const auto &name : street_names) {
+            nameToCount.insert({name, 0});
+        }
+    }
+
+    void insertCarInit(const std::string &name) {
+        nameToCount[name]++;
+    }
+
+    void print() {
+        std::vector<std::pair<std::string, int>> tmp;
+        for (auto &p : nameToCount) {
+            if (p.second != 0) {
+                tmp.push_back(p);
+            }
+        }
+
+        std::sort(tmp.begin(), tmp.end(), [](auto a, auto b) { return a.second > b.second; });
+        for (auto &p : tmp) {
+            std::cout << p.first << " " << p.second << std::endl;
+            street_names_used.erase(p.first);
+        }
+
+		for (const auto& sn : street_names_used) {
+			std::cout << sn << " 1" << '\n';
+		}
+    }
 };
 
 int main()
@@ -45,6 +76,10 @@ int main()
 		junctions[s.end].street_names.push_back(s.name);
 	}
 
+    for (Intersection &inter : junctions) {
+        inter.initToCount();
+    }
+
     for (int i = 0; i < V; i++) {
         int count;
         std::cin >> count;
@@ -52,12 +87,19 @@ int main()
         for (int j = 0; j < count - 1; j++) {
             std::cin >> name;
             junctions[streetToIntersection[name]].street_names_used.insert(name);
+            if (j == 0) {
+                junctions[streetToIntersection[name]].insertCarInit(name);
+            }
         }
         std::cin >> name;
+        if (count == 1) {
+            junctions[streetToIntersection[name]].insertCarInit(name);
+        }
+
     }
 
     int outI = 0;
-	for (int i = 0 ; i < I ; i++ ) {
+	for (int i = 0 ; i < I ; i++) {
         if (junctions[i].street_names_used.size() != 0) {
             outI++;
         }
@@ -72,9 +114,7 @@ int main()
 		std::cout << i << '\n' 
 			<< junctions[i].street_names_used.size() 
 			<< '\n';
-		for (const auto& sn : junctions[i].street_names_used){
-			std::cout << sn << " 1" << '\n';
-		}
+        junctions[i].print();
 	}
 
 	return 0;
